4159bfb918d8c1d859da8fc5524fa42a90379726
Philip T Dodd <phil.dodd@cinnamonapps.co.uk>
2023-02-19 13:26:13 +0000

    red: initial test fails

--- src/test/java/org/example/TheatreSeatsTest.java
+++ src/test/java/org/example/TheatreSeatsTest.java
@@ -1,4 +1,14 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TheatreSeatsTest {
+    @Test
+    void getAvailableSeats() {
+        TheatreSeats theatreSeats = new TheatreSeats(3, 5);
+
+        assertEquals(15, theatreSeats.getAvailableSeats());
+    }
 }
